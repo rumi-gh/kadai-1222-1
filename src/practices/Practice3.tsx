d50a976353f2b42fe5b891514a4dc05189710604
rumi-gh <147528614+rumi-gh@users.noreply.github.com>
2023-12-21 15:14:19 +0900

    Add files via upload

---
+++ src/practices/Practice3.tsx
@@ -0,0 +1,21 @@
+export const Practice3 = () => {
+  const getTotalFee = (num: number) => {
+    const total = num * 1.1;
+    return total;
+    // return total.toString;
+    // エラーになる
+  };
+
+  const onClickPractice = () => {
+    let total: number = 0;
+    total = getTotalFee(1000);
+    console.log(total);
+  };
+
+  return (
+    <div>
+      <p>練習問題：変数の型指定</p>
+      <button onClick={onClickPractice}>練習問題3を実行</button>
+    </div>
+  );
+};
